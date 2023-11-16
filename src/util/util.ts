0d41f525bd2ee0ebcc79181f977f81d96526f8db
Wale Moren <i@owale.co>
2023-11-16 08:38:12 +0000

    initial commit

---
+++ src/util/util.ts
@@ -0,0 +1,14 @@
+export const numberToColor = (min: number, max: number, value: number) => {
+  // Ensure value is within range
+  value = Math.min(Math.max(value, min), max);
+
+  // Calculate the ratio of the value
+  const ratio = (value - min) / (max - min);
+
+  // Calculate green and red values
+  const green = Math.floor((1 - ratio) * 255);
+  const red = Math.floor(ratio * 255);
+
+  // Return the RGB color string
+  return `rgb(${red},${green},0)`;
+}
